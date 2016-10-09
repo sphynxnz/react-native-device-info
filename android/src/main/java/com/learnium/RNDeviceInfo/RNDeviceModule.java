54dc85aff64d5db2b9838aad365e9266ca5eafd0
Ferdinand Contreras <flpcnz@gmail.com>
2016-10-09 15:02:55 +1300

    Updated RNDeviceModule.java to generate UUID v3 from Secure.ANDROID_ID and use the generated UUID as the CN for the certificate stored in the AndroidKeyStore under the alias name of "lnz-uuid". If generation of UUID v3 from Secure.ANDROID_ID fails, UUID.randomUUID() is used instead.
    Future retrieval of UUID relies on the CN saved with certificate under the said alias. This UUID
    will survive app uninstall for Android versions earlier than Lollipop, but not later versions inclusive
    of Lollipop. If the UUID is successfully generated from Secure.ANDROID_ID, then the UUID will remain the
    same regardless of app uninstall/reinstall, unless a factory reset is done.

--- android/src/main/java/com/learnium/RNDeviceInfo/RNDeviceModule.java
+++ android/src/main/java/com/learnium/RNDeviceInfo/RNDeviceModule.java
@@ -15,8 +15,19 @@ import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
 import java.util.TimeZone;
+import java.util.UUID;
 
 import javax.annotation.Nullable;
+
+import java.security.KeyStore;
+import java.security.cert.X509Certificate;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import javax.security.auth.x500.X500Principal;
+import android.security.keystore.KeyProperties;
+import android.security.keystore.KeyGenParameterSpec;
+import android.security.keystore.KeyGenParameterSpec.Builder;
+import java.util.Calendar;
 
 public class RNDeviceModule extends ReactContextBaseJavaModule {
 
@@ -89,11 +100,81 @@ public class RNDeviceModule extends ReactContextBaseJavaModule {
     constants.put("deviceId", Build.BOARD);
     constants.put("deviceLocale", this.getCurrentLanguage());
     constants.put("deviceCountry", this.getCurrentCountry());
-    constants.put("uniqueId", Secure.getString(this.reactContext.getContentResolver(), Secure.ANDROID_ID));
+    //constants.put("uniqueId", Secure.getString(this.reactContext.getContentResolver(), Secure.ANDROID_ID));
+    constants.put("uniqueId", getLNZUUID());
+    //constants.put("uniqueId", getLNZUUID();
     constants.put("systemManufacturer", Build.MANUFACTURER);
     constants.put("bundleId", packageName);
     constants.put("userAgent", System.getProperty("http.agent"));
     constants.put("timezone", TimeZone.getDefault().getID());
     return constants;
+  }
+
+  /*
+  * This generates the UUID based on the Secure.ANDROID_ID which is a hex string (length=16).
+  * The resulting string from the Secure.ANDROID_ID is compared with the hex string "9774d56d682e549c"
+  * which is related to an issue with generating unique Secure.ANDROID_ID as described in the followingn
+  * link: https://code.google.com/p/android/issues/detail?id=10603
+  * 
+  * The code below is based on discussions from here:
+  * http://stackoverflow.com/questions/2785485/is-there-a-unique-android-device-id/5626208#5626208
+  *
+  * The UUID is generated from the byte array version of the ANDROID_ID and is of version 3 UUID. 
+  * If an exception if encountered, the UUID falls back to just generating a random UUID version 4. 
+  * The caveat is that, this version 4 UUID will not survive across app install/uninstall/reinstall
+  * process.
+  */
+  private String getUUIDfromAndroidId() {
+    String androidId = Secure.getString(this.reactContext.getContentResolver(), Secure.ANDROID_ID);
+    String uuid = "";
+    if (!"9774d56d682e549c".equals(androidId)) {
+      try {
+        byte[] byteArray = androidId.getBytes("UTF-8");
+        uuid = UUID.nameUUIDFromBytes(byteArray).toString();
+      } catch (Exception e) {
+        uuid = UUID.randomUUID().toString();
+      }
+    }
+    return (uuid);
+  }
+
+  /*
+  * Retrieve generated UUID from AndroidKeyStore using alias "lnz-uuid". The UUID is actually the
+  * subject name of the saved certificate (e.g. string after CN=...) if the alias is found in the keystore.
+  *
+  * Otherwise, uses getUUIDFromAndroid() to initially generate the UUID and use it as the CN name of the 
+  * certificate in the keystore associated with the alias "lnz-uuid". This is a bit long-winded and kind of
+  * a of lame attempt to align with the KeyChain approach use din iOS - the generated UUID could have easily
+  * been returned from getUUIDfromAndroidId(). 
+  */
+  private String getLNZUUID() {
+    String alias = "lnz-uuid";
+    String uuid = getUUIDfromAndroidId();
+    try {
+      KeyStore keystore = KeyStore.getInstance("AndroidKeyStore");
+      keystore.load(null);      // Create new key if needed
+      if (!keystore.containsAlias(alias)) {
+        Calendar start = Calendar.getInstance();
+        Calendar end = Calendar.getInstance();
+        end.add(Calendar.YEAR, 1);
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance(
+          KeyProperties.KEY_ALGORITHM_RSA, "AndroidKeyStore");
+        keyPairGenerator.initialize(
+          new KeyGenParameterSpec.Builder(alias, KeyProperties.PURPOSE_SIGN)
+            .setCertificateSubject(new X500Principal("CN="+uuid))
+            .setKeyValidityStart(start.getTime())
+            .setKeyValidityEnd(end.getTime())
+            .build());
+        KeyPair keyPair = keyPairGenerator.generateKeyPair();
+      } else {
+        // Retrieve alias entry
+        X509Certificate cert = ((X509Certificate) keystore.getCertificate(alias));  
+        // Return subject name - starts after CN=...      
+        uuid = cert.getIssuerX500Principal().getName().substring(3);
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    return uuid;
   }
 }
